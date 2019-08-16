49b31f62c4819e8867c596a6ee944cabf8686547
sunjk <sunjk@123.com>
2019-08-16 14:19:03 +0800

    111111

---
+++ src/main/java/com/example/gitdemo/test1.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo;
+
+/**
+ * @program: gitdemo
+ * @description: test1
+ * @author: sjk
+ * @create: 2019-08-16 14:17
+ */
+public class test1 {
+    private String 属性1版本1;
+    private String 属性2版本1;
+
+
+}
