6784a82918a5a2743e804ff7bf2b1ad93b3e415b
sunjk <sunjk@123.com>
2019-08-16 17:17:19 +0800

    test2

---
+++ src/main/java/com/example/gitdemo/Test2.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo;
+
+/**
+ * @program: gitdemo
+ * @description: test2
+ * @author: sjk
+ * @create: 2019-08-16 17:15
+ */
+public class Test2 {
+    private String aaa;
+
+}
